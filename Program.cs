1f7a4a8e811514649cba83ba2dc095b17b43e4d6
MerBasNik <125216886+MerBasNik@users.noreply.github.com>
2023-08-23 01:23:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        private static void SortNums(List<string> StrOfNums)
+        {
+            for (int i = 0; i < StrOfNums.Count - 1; i++)
+                for (int j = 0; j < StrOfNums.Count - i - 1; j++)
+                    if ( int.Parse(StrOfNums[j]) > int.Parse(StrOfNums[j+1]) )
+                    {
+                        string Temp = StrOfNums[j];
+                        StrOfNums[j] = StrOfNums[j + 1];
+                        StrOfNums[j + 1] = Temp;
+                    }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа Bubble Sort\n\rВведите строку чисел...\n\r");
+            List<string> StrOfNums = new List<string>(Console.ReadLine().Split(' '));
+            SortNums(StrOfNums);
+
+            foreach (string num in StrOfNums)
+            {
+                Console.Write(num + ' ');
+            }
+        }
+    }
+}
